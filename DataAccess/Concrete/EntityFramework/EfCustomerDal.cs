fb83df2f2f52c1bd7a2c3fde7fae069099e42ec9
Birkankaraer <karaermustafabirkan@gmail.com>
2023-10-05 12:32:20 +0300

    Update

--- DataAccess/Concrete/EntityFramework/EfCustomerDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCustomerDal.cs
@@ -1,6 +1,7 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,26 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EfCustomerDal : EfEntityRepositoryBase<Customer, RentACarContext>, ICustomerDal
     {
+        public List<CustomerDetailDto> GetCustomerDetails()
+        {
+            using (RentACarContext context = new RentACarContext())
+            {
+                var customerDetail = from c in context.Customers
+                                     join u in context.Users
+                                     on c.UserId equals u.UserId
+                                     select new CustomerDetailDto
+                                     {
+                                         CompanyName = c.CompanyName,
+                                         CustomerId = c.CustomerId,
+                                         Email = u.Email,
+                                         FirstName = u.FirstName,
+                                         LastName = u.LastName
+                                     };
+                return customerDetail.ToList();
 
+
+
+            }
+        }
     }
 }
