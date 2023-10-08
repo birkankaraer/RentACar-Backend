bb006df25d3c8e25c2a0fc9aeef3889ac5aa14ce
Birkankaraer <karaermustafabirkan@gmail.com>
2023-10-09 01:08:33 +0300

    AOP

--- Core/Utilities/Interceptors/AspectInterceptorSelector.cs
+++ Core/Utilities/Interceptors/AspectInterceptorSelector.cs
@@ -18,7 +18,8 @@ namespace Core.Utilities.Interceptors
             var methodAttributes = type.GetMethod(method.Name)
                 .GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
             classAttributes.AddRange(methodAttributes);
-            
+            //classAttributes.Add(new ExceptionLogAspect(typeof(FileLogger))); //loglama alt yapısı yazıldıktan sonra bu eklenebilir
+
 
             return classAttributes.OrderBy(x => x.Priority).ToArray();
         }
