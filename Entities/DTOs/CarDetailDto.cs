b48a52562c8ac3a7d0a9ec44dd72c49109a8760a
Birkankaraer <karaermustafabirkan@gmail.com>
2023-10-25 23:53:17 +0300

    code refactoring

--- Entities/DTOs/CarDetailDto.cs
+++ Entities/DTOs/CarDetailDto.cs
@@ -14,7 +14,6 @@ namespace Entities.DTOs
         public string CarName { get; set; }        
         public string BrandName { get; set; }
         public string ColorName { get; set; }
-        public string ModelName { get; set; }
         public int ModelYear { get; set; }
         public decimal DailyPrice { get; set; }
         public string Description { get; set; }
