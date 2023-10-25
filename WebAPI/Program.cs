6f7f2d58a934b2479b9d383f8336eb7815783ac8
Birkankaraer <karaermustafabirkan@gmail.com>
2023-10-25 23:52:08 +0300

    Updates

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -63,6 +63,8 @@ namespace WebAPI
                 app.UseSwaggerUI();
             }
 
+            app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
+
             app.UseHttpsRedirection();
 
             app.UseAuthentication();
